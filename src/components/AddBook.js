05c200d0b741c875d896ee828b87f23591f477c8
isaacpitwa <isaacpitwa256@gmail.com>
2022-05-03 19:17:10 +0300

    Add book

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addNewBook } from '../redux/books/books';
 
 function AddBook() {
   const [inputText, setInputText] = useState({
     title: '',
     author: '',
   });
+  const dispatch = useDispatch();
 
   const onChange = (e) => {
     setInputText({
@@ -15,6 +19,15 @@ function AddBook() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    dispatch(addNewBook({
+      id: uuidv4(),
+      title: inputText.title,
+      author: inputText.author,
+    }));
+    setInputText({
+      title: '',
+      author: '',
+    });
   };
 
   return (
@@ -28,14 +41,16 @@ function AddBook() {
           value={inputText.title}
           name="title"
           onChange={onChange}
+          required
         />
         <input
           type="text"
           className="input-text"
           placeholder="Author"
           value={inputText.author}
-          name="title"
+          name="author"
           onChange={onChange}
+          required
         />
         <button type="submit"> Add Book</button>
       </form>
